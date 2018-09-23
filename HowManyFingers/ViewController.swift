e65c40c0d2b6798e7ce55ae24d5b2d5ee2026177
Tarek Moubarak <tarek@hellotree.co>
2018-09-23 13:30:54 +0300

    Full App

--- HowManyFingers/ViewController.swift
+++ HowManyFingers/ViewController.swift
@@ -9,7 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var userInput: UITextField!
+    @IBOutlet weak var resultLbl: UILabel!
+    
+    @IBAction func guessBtn(_ sender: Any) {
+        let randomNumber = Int.random(in: 0 ..< 10)
+        //OR
+//        let randomNumber = String(arc4random_uniform(10));
+//        if (randomNumber == userInput.text){
+        if (randomNumber == Int(userInput.text!)){
+            resultLbl.text = "Correct!"
+        } else{
+            resultLbl.text = "Wrong! Correct number is \(randomNumber)"
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
